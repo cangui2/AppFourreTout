498a61a03447671c7763fb8c00228d598bc1335d
cangui <canguijc@gmail.com>
2021-04-08 19:09:25 +0200

    covid ok bug a resoudre

--- App.js
+++ App.js
@@ -9,22 +9,61 @@ import Result from './component/result';
 import OpenFood from './page/OpenFood';
 import LoveCalculator from './page/LoveCalculator';
 import Covid from './page/Covid';
+import CardButton from '@paraboly/react-native-card-button';
 
 function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
+      <Text>Plein de petit programme tous application</Text>
+      {/*<Button*/}
+      {/*  title="Go to Details"*/}
+      {/*  onPress={() => navigation.navigate('Details')}*/}
+      {/*/>*/}
+      {/*<Button*/}
+      {/*  title="Go to Details"*/}
+      {/*  onPress={() => navigation.navigate('love')}*/}
+      {/*/>*/}
+      {/*<Button*/}
+      {/*  title="Go to Details"*/}
+      {/*  onPress={() => navigation.navigate('Covid Menu')}*/}
+      {/*/>*/}
+      <CardButton
         onPress={() => navigation.navigate('Details')}
+        text="Yuka"
+        iconSize={34}
+        iconColor="white"
+        textColor="white"
+        iconType="favorite"
+        rippleColor="white"
+        end={{x: 1, y: 1}}
+        start={{x: 0, y: 0}}
+        gradientColors={['#BA5370', '#F4E2D8']}
       />
-      <Button
-        title="Go to Details"
+      <CardButton
         onPress={() => navigation.navigate('love')}
+        text="Love calculator"
+        iconSize={34}
+        iconColor="white"
+        textColor="white"
+        iconType="
+settings_accessibility
+"
+        rippleColor="white"
+        end={{x: 1, y: 1}}
+        start={{x: 0, y: 0}}
+        gradientColors={['#3368ff', '#F4E2D8']}
       />
-      <Button
-        title="Go to Details"
+      <CardButton
         onPress={() => navigation.navigate('Covid Menu')}
+        text="Covid Menu"
+        iconSize={34}
+        iconColor="white"
+        textColor="white"
+        iconType="wifi"
+        rippleColor="white"
+        end={{x: 1, y: 1}}
+        start={{x: 0, y: 0}}
+        gradientColors={['#ff3933', '#F4E2D8']}
       />
     </View>
   );
